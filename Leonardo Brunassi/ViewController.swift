112c8d3ef54dae2579833c370003506ef10f61b4
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-04-15 12:34:32 -0300

    Primeiros Commits

---
+++ Leonardo Brunassi/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Leonardo Brunassi
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 15/04/15.
+//  Copyright (c) 2015 Leonardo Rodrigues de Morais Brunassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageProfile: UIButton!
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Deixando a imagem de perfil redonda.
+        imageProfile.layer.borderColor = UIColor.whiteColor().CGColor
+        imageProfile.layer.cornerRadius = 74
+        imageProfile.clipsToBounds = true
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
